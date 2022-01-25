c8306ab36a3f28e81a4a6ca8d63b0022f12f483e
vlad1581 <vlad.art.91@mail.ru>
2022-01-25 23:42:15 +0300

    commit and push

---
+++ src/Main.java
@@ -0,0 +1,9 @@
+public class Main {
+    public static void Main(String[] Args) {
+        int i;
+        for (i = 1; i < 10; i++) {
+            System.out.println("Hello");
+        }
+    }
+
+}
